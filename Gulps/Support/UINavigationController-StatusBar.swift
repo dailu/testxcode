544c0836296a2b592aec9802084bc452873e8760
dailu <dai_lu@hotmail.com>
2017-01-01 15:28:21 +0800

    remove comments

--- Gulps/Support/UINavigationController-StatusBar.swift
+++ Gulps/Support/UINavigationController-StatusBar.swift
@@ -1,9 +1,6 @@
 import UIKit
 
-/**
- Keeps the navigation bar hidden in the onboarding controllers
- and visible in the others, with a light status bar
- */
+
 extension UINavigationController {
   open override var preferredStatusBarStyle : UIStatusBarStyle {
     return .lightContent
